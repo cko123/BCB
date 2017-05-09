3aeefe3d9c2495578b4bb06adb4d34d44a07ee5c
Christoph H. Kopka <christoph@dk-it.eu>
2017-05-09 21:12:25 +0200

    Initial commit

---
+++ app/src/main/java/eu/dk_it/chrishk/bcb/Project1MainActivity.java
@@ -0,0 +1,13 @@
+package eu.dk_it.chrishk.bcb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Project1MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project1_main);
+    }
+}
